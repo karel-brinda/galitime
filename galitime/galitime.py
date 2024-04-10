12d76f69aab5a82378809ca2bcec4d8f137cdeb8
Karel Břinda <karel.brinda@gmail.com>
2024-04-10 10:59:01 +0200

    Add a simple timing test

--- galitime/galitime.py
+++ galitime/galitime.py
@@ -20,6 +20,7 @@ def get_args():
     parser.add_argument('command', help='The command to be benchmarked')
     parser.add_argument(
         '--log',
+        required=True,
         help='Path to the log file with benchmark statistics (if the directory doesn\'t exist, it will be created).'
     )
     parser.add_argument('--experiment', help='Name of the experiment (to be attached to the output)')
